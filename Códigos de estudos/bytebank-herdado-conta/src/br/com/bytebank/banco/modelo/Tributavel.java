3c70f3cfbb7d16e95bd561bcdcddef5669d44bac
Mari <maritlach50@gmail.com>
2022-05-01 14:03:57 -0300

    retirada teste

--- Códigos de estudos/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Tributavel.java
+++
@@ -1,7 +0,0 @@
-package br.com.bytebank.banco.modelo;
-
-public interface Tributavel {
-	
-	double getValorImposto(); //pode ser escrito sem o public abstract pois ja está implicito para o compilador.
-	
-}
