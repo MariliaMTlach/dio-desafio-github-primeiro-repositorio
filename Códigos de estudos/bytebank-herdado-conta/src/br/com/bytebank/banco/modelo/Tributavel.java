9b4a6a39f3d223e73fbd63eda62317247e4ed4f8
Mari <maritlach50@gmail.com>
2022-05-01 13:56:46 -0300

    Adição da pasta Códigos de Estudo, código do Bytebank estudo de código do curso da alura

---
+++ Códigos de estudos/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Tributavel.java
@@ -0,0 +1,7 @@
+package br.com.bytebank.banco.modelo;
+
+public interface Tributavel {
+	
+	double getValorImposto(); //pode ser escrito sem o public abstract pois ja está implicito para o compilador.
+	
+}
